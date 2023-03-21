e19425c0bc1f2b6c69eb9412f2411d8032e27bca
danielenovarese99 <s319508@studenti.polito.it>
2023-03-21 10:29:03 +0100

    answers.html exercise >> adding html elements with js

--- lesson_exercise/index.js
+++ lesson_exercise/index.js
@@ -1,2 +1,20 @@
 'use strict';
-console.log("Hello world");
+
+
+
+
+
+// get user input with document.getelementsbyclassname and save to variables
+// create HTML elements and then append at the end of the tabel 
+function addInputElements() {
+    const newRow = document.createElement('tr');
+    Array.from(document.getElementsByClassName("input1")).forEach(element => {
+        let newTD = document.createElement('td');
+        let text = document.createTextNode(element.value);
+        newTD.append(text);
+        newRow.append(newTD);
+    });
+    // document.getElementsByClassName("tableBody1")[0].append(newRow);
+    document.getElementsByClassName("lastHC")[0].after(newRow);
+}
+
