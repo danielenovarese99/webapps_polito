e6eb9d086930d3b2925dc272da1dd2c4798b8831
danielenovarese99 <s319508@studenti.polito.it>
2023-03-26 16:57:40 +0200

    corrected a few grid layout things - started using col-lg, col-md, col-sm also removed desktop / mobile added sample static movies + filters and effects for choosing certain filters (later on modify functions to filter the movies shown)

--- third_lab/js/index.js
+++ third_lab/js/index.js
@@ -1,11 +1,27 @@
 'use strict';
 
 
-function toggleShadow(){
-    document.getElementsByClassName("floatingButtonDiv")[0].classList.remove("buttonShadow");
+function toggleShadow() {
+    document.getElementsByClassName("addMovieContainer")[0].classList.remove("buttonShadow");
     setTimeout(() => {
-        document.getElementsByClassName("floatingButtonDiv")[0].classList.add("buttonShadow")
-    },50);
+        document.getElementsByClassName("addMovieContainer")[0].classList.add("buttonShadow")
+    }, 100);
     // TODO >> prompt user with window to insert new movie
 }
+const filterOptions = Array.from(document.getElementsByClassName('filterOption1'));
+filterOptions.forEach(element => {
+    element.onclick = () => {
+        filterOptions.forEach(element2 => element2.classList.remove('selected1'));
+        filterOptions.forEach(element2 => element2.classList.add('text-muted'));
+        element.classList.remove('text-muted');
+        element.classList.add('selected1');
+    }
+});
 
+const filterOptions2 = Array.from(document.getElementsByClassName('filterOption2'));
+filterOptions2.forEach(element => {
+    element.onclick = () => {
+        filterOptions2.forEach(element2 => element2.classList.remove('selected2'));
+        element.classList.add('selected2');
+    }
+});
