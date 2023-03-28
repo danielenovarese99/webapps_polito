02299b0d4d80270e3ac22b7e7365dd12ba8432e5
danielenovarese99 <s319508@studenti.polito.it>
2023-03-28 09:45:46 +0200

    added dom event onready >> laod script

--- third_lab/js/index.js
+++ third_lab/js/index.js
@@ -1,33 +1,36 @@
 'use strict';
 
+document.addEventListener("DOMContentLoaded", ready => {
 
-function toggleShadow() {
-    document.getElementsByClassName("addMovieContainer")[0].classList.remove("buttonShadow");
-    setTimeout(() => {
-        document.getElementsByClassName("addMovieContainer")[0].classList.add("buttonShadow")
-    }, 100);
-    // TODO >> prompt user with window to insert new movie
-}
-
-
-const checkBoxes = Array.from(document.getElementsByClassName("checkBoxInput"));
-checkBoxes.forEach(element => element.checked = true);
-
-
-const filterOptions = Array.from(document.getElementsByClassName('filterOption1'));
-filterOptions.forEach(element => {
-    element.onclick = () => {
-        filterOptions.forEach(element2 => element2.classList.remove('selected1'));
-        filterOptions.forEach(element2 => element2.classList.add('text-muted'));
-        element.classList.remove('text-muted');
-        element.classList.add('selected1');
+    function toggleShadow() {
+        document.getElementsByClassName("addMovieContainer")[0].classList.remove("buttonShadow");
+        setTimeout(() => {
+            document.getElementsByClassName("addMovieContainer")[0].classList.add("buttonShadow")
+        }, 100);
+        // TODO >> prompt user with window to insert new movie
     }
-});
 
-const filterOptions2 = Array.from(document.getElementsByClassName('filterOption2'));
-filterOptions2.forEach(element => {
-    element.onclick = () => {
-        filterOptions2.forEach(element2 => element2.classList.remove('selected2'));
-        element.classList.add('selected2');
-    }
-});
+
+    const checkBoxes = Array.from(document.getElementsByClassName("checkBoxInput"));
+    checkBoxes.forEach(element => element.checked = true);
+
+
+    const filterOptions = Array.from(document.getElementsByClassName('filterOption1'));
+    filterOptions.forEach(element => {
+        element.onclick = () => {
+            filterOptions.forEach(element2 => element2.classList.remove('selected1'));
+            filterOptions.forEach(element2 => element2.classList.add('text-muted'));
+            element.classList.remove('text-muted');
+            element.classList.add('selected1');
+        }
+    });
+
+    const filterOptions2 = Array.from(document.getElementsByClassName('filterOption2'));
+    filterOptions2.forEach(element => {
+        element.onclick = () => {
+            filterOptions2.forEach(element2 => element2.classList.remove('selected2'));
+            element.classList.add('selected2');
+        }
+    });
+
+})
