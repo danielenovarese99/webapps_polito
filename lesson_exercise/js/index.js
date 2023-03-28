0cf038d48e8ed57641b7ebcaa5fdc8f41c5e7707
danielenovarese99 <s319508@studenti.polito.it>
2023-03-28 10:38:47 +0200

    Completed class exercise about dynamic HTML generation from an array of objects. (DOM + Node manipulation) + window.load event for loading such dynamic nodes Note: FUNCTIOS go outside of window.load

--- lesson_exercise/js/index.js
+++ lesson_exercise/js/index.js
@@ -1,8 +1,44 @@
 'use strict';
 
+window.onload = function () {
+
+    const answers = [
+        { date: "15/02/2023", text: "No", author: "daniele", score: 10 },
+        { date: "17/02/2023", text: "Yes", author: "daniele", score: -10 },
+        { date: "15/02/2023", text: "No", author: "daniele", score: 10 },
+        { date: "15/02/2023", text: "No", author: "daniele", score: 10 },
+        { date: "15/02/2023", text: "No", author: "daniele", score: 10 },
+    ];
+
+    const tableBody1 = document.getElementsByClassName('tableBody1')[0];
+    answers.forEach(element => {
+        let newRow = document.createElement('tr');
+        Object.keys(element).forEach(el => {
+            if (element == 'score') {
+                let newTD = document.createELement('td');
+                newTD.innerText = element[el];
+                newTD.classList.add('scoreCounter');
+                newRow.appendChild(newTD);
+            }
+            else {
+                let newTD = document.createElement('td');
+                newTD.innerText = element[el];
+                newRow.appendChild(newTD);
+            }
+        })
+        let btn = document.createElement('button');
+        btn.innerText = 'Increase';
+        btn.addEventListener('click', () => {
+            btn.previousSibling.innerText = Number(btn.previousSibling.innerText) + 1;
+        });
 
 
+        newRow.appendChild(btn);
+        tableBody1.appendChild(newRow);
+    })
 
+
+};
 
 // get user input with document.getelementsbyclassname and save to variables
 // create HTML elements and then append at the end of the tabel 
@@ -15,6 +51,5 @@ function addInputElements() {
         newRow.append(newTD);
     });
     // document.getElementsByClassName("tableBody1")[0].append(newRow);
-    document.getElementsByClassName("lastHC")[0].after(newRow);
+    document.getElementsByClassName('tableBody1')[0].appendChild(newRow);
 }
-
